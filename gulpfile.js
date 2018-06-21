2b795b79732a34f73f1c5ba661a9460a22dce73d
Satyajit Rout <satyajitrout@Satyajits-MacBook-Pro-2.local>
2018-06-21 19:23:42 +0530

    bootstrap 4 added

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var browserSync = require('browser-sync');
 var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
-var cssnano = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
 var del = require('del');
@@ -60,7 +59,6 @@ gulp.task('useref', function() {
   return gulp.src('app/*.html')
     .pipe(useref())
     .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist'));
 });
 
